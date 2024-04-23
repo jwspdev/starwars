efdb8e5c6d3f5b06371803a3b06ac403d653e3ca
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-04-24 00:40:26 +0800

    Home/Data/Data Sources/Remote (Service and Source)
    - Service: Implemented a Remote Retrofit Api Service for People
    - Service: Added 2 Methods: listing of people and getting of current person
    - Service: Utilized Retrofit Generator for the remote people api service generated implementation
    - Source: Implemented a data source for actual implementation of the services mentioned (to be used in future testing as well)

---
+++ lib/src/modules/home/data/data_sources/remote/service/remote_people_api_service.g.dart
@@ -0,0 +1,108 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'remote_people_api_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _RemotePeopleApiService implements RemotePeopleApiService {
+  _RemotePeopleApiService(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'https://swapi.py4e.com/api/';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<HttpResponse<ListPeopleResponse>> listPeople(int pageNumber) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'page': pageNumber};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<HttpResponse<ListPeopleResponse>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'people/',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = ListPeopleResponse.fromJson(_result.data!);
+    final httpResponse = HttpResponse(value, _result);
+    return httpResponse;
+  }
+
+  @override
+  Future<HttpResponse<Person>> getCurrentPerson(int id) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<HttpResponse<Person>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'people/${id}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = Person.fromJson(_result.data!);
+    final httpResponse = HttpResponse(value, _result);
+    return httpResponse;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
---
+++ lib/src/modules/home/data/data_sources/remote/source/remote_people_data_source.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:retrofit/dio.dart';
+
+import 'package:star_wars_app/src/modules/home/data/data_sources/remote/service/remote_people_api_service.dart';
+import 'package:star_wars_app/src/modules/home/data/models/person.dart';
+import 'package:star_wars_app/src/modules/home/data/models/responses/list_people_response.dart';
+
+abstract class RemotePeopleDataSource {
+  Future<HttpResponse<ListPeopleResponse>> listPeople(int pageNumber);
+  Future<HttpResponse<Person>> getCurrentPerson(int id);
+}
+
+class RemotePeopleDataSourceImpl implements RemotePeopleDataSource {
+  RemotePeopleApiService apiService;
+  RemotePeopleDataSourceImpl({
+    required this.apiService,
+  });
+  @override
+  Future<HttpResponse<Person>> getCurrentPerson(int id) {
+    return apiService.getCurrentPerson(id);
+  }
+
+  @override
+  Future<HttpResponse<ListPeopleResponse>> listPeople(int pageNumber) {
+    return apiService.listPeople(pageNumber);
+  }
+}
