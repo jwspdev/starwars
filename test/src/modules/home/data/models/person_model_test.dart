d41a6cfb055391700795349c99a424bfe7fe0a99
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-04-24 01:50:23 +0800

    flutter test
    - added films, vehicles, and starships for testpersonmodel

--- test/src/modules/home/data/models/person_model_test.dart
+++ test/src/modules/home/data/models/person_model_test.dart
@@ -10,15 +10,31 @@ import '../../../../../helpers/json_reader.dart';
 
 void main() {
   const testPersonModel = Person(
-      name: "Luke Skywalker",
-      height: 172,
-      mass: 77,
-      hairColor: "blond",
-      skinColor: "fair",
-      eyeColor: 'blue',
-      birthYear: '19BBY',
-      gender: 'male',
-      url: 'https://swapi.py4e.com/api/people/1/');
+    name: "Luke Skywalker",
+    height: 172,
+    mass: 77,
+    hairColor: "blond",
+    skinColor: "fair",
+    eyeColor: 'blue',
+    birthYear: '19BBY',
+    gender: 'male',
+    url: 'https://swapi.py4e.com/api/people/1/',
+    films: [
+      "https://swapi.py4e.com/api/films/1/",
+      "https://swapi.py4e.com/api/films/2/",
+      "https://swapi.py4e.com/api/films/3/",
+      "https://swapi.py4e.com/api/films/6/",
+      "https://swapi.py4e.com/api/films/7/"
+    ],
+    vehicles: [
+      "https://swapi.py4e.com/api/vehicles/14/",
+      "https://swapi.py4e.com/api/vehicles/30/"
+    ],
+    starships: [
+      "https://swapi.py4e.com/api/starships/12/",
+      "https://swapi.py4e.com/api/starships/22/"
+    ],
+  );
 
   //check if inherits personentity
   test('should be a subclass of person entity', () async {
