3b1aa48de4d52d0eb3c4ed695d872fac67786841
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-04-24 21:37:35 +0800

    Test Data Sources
    - fixed imports for people api service test
    - created a test for api calls that checks if it should return list of starship response and if it should return starship model

--- test/src/modules/home/data/data_sources/remote/service/remote_people_api_service_test.dart
+++ test/src/modules/home/data/data_sources/remote/service/remote_people_api_service_test.dart
@@ -4,8 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:retrofit/retrofit.dart';
-import 'package:star_wars_app/src/modules/home/data/data_sources/remote/service/remote_people_api_service.dart';
-import 'package:star_wars_app/src/modules/home/data/data_sources/remote/source/remote_people_data_source.dart';
+import 'package:star_wars_app/src/modules/home/data/data_sources/remote/people/service/remote_people_api_service.dart';
+import 'package:star_wars_app/src/modules/home/data/data_sources/remote/people/source/remote_people_data_source.dart';
 import 'package:star_wars_app/src/modules/home/data/models/person.dart';
 import 'package:star_wars_app/src/modules/home/data/models/responses/list_people_response.dart';
 
---
+++ test/src/modules/home/data/data_sources/remote/source/remote_starship_data_source_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:star_wars_app/src/dependency_injection/injection_container.dart';
+import 'package:star_wars_app/src/modules/home/data/data_sources/remote/starships/source/remote_starship_data_source.dart';
+import 'package:star_wars_app/src/modules/home/data/models/responses/list_starships_response.dart';
+import 'package:star_wars_app/src/modules/home/data/models/starship.dart';
+import 'package:star_wars_app/src/modules/home/domain/entities/responses/list_starship_response_entity.dart';
+
+void main() {
+  late RemoteStarshipDataSourceImpl dataSource;
+  setUp(() {
+    initializeDependencies();
+    dataSource = RemoteStarshipDataSourceImpl(apiService: sl());
+  });
+
+  //starship api call tests
+  group('starship api data source test', () {
+    const testStarshipModel = Starship(
+        hyperdriveRating: 2.0,
+        mglt: 60,
+        starshipClass: "corvette",
+        name: "CR90 corvette",
+        model: "CR90 corvette",
+        manufacturer: "Corellian Engineering Corporation",
+        costInCredits: "3500000",
+        length: "150",
+        maxAtmospheringSpeed: "950",
+        crew: "30-165",
+        passengers: "600",
+        cargoCapacity: "3000000",
+        consumables: "1 year",
+        pilots: [],
+        films: [
+          "https://swapi.py4e.com/api/films/1/",
+          "https://swapi.py4e.com/api/films/3/",
+          "https://swapi.py4e.com/api/films/6/"
+        ],
+        url: "https://swapi.py4e.com/api/starships/2/");
+    test('should return list of starship response', () async {
+      //arrange
+      int page = 1;
+      //act
+      final result = await dataSource.listStarships(page);
+      //assert
+      debugPrint('COUNT: ${result.data.count}');
+      debugPrint('NEXT: ${result.data.next}');
+      debugPrint('PREVIOUS: ${result.data.previous}');
+      for (var starship in result.data.results) {
+        debugPrint('$starship');
+      }
+      //assert
+      expect(result.data, isA<ListStarshipsResponse>());
+      expect(result.data, isA<ListStarshipsResponseEntity>());
+    });
+    test('should return starship model', () async {
+      //arrange
+      int id = 2;
+      //act
+      final apiResult = await dataSource.getCurrentStarship(id);
+      final apiStarshipResult = apiResult.data;
+      final apiStarshipResultEntity = apiStarshipResult.toEntity();
+      //assert
+      expect(apiStarshipResult, equals(testStarshipModel));
+      expect(apiStarshipResultEntity, equals(testStarshipModel.toEntity()));
+    });
+  });
+}
