1f161836105a4097087d9e2779bf8bdaee0277a3
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-05-04 01:21:46 +0800

    Core: Utils
    - Initialized Custom Colors
    - Added Navigation Tabs for constants
    - Fixed String Extension get Id from URL: issue: what if the length of the id from the url is > 1

---
+++ lib/src/core/utils/custom_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color primaryColor = Color.fromRGBO(30, 54, 137, 1);
+  static const Color tertiaryColor = Color.fromRGBO(49, 102, 163, 1);
+}
