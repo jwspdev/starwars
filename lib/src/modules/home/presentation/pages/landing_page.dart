de6a61a471e7e0f3c3dff3a9bbd563b85d067531
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-05-04 01:13:23 +0800

    Pages: Landing and Home Page
    - Landing: implemented landing page that houses the general page that contains app bar and a bottom navigation bar (all functional)
    - Home: Implemented with custom top navigation which is functional as well

---
+++ lib/src/modules/home/presentation/pages/landing_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:star_wars_app/src/core/utils/custom_colors.dart';
+import 'package:star_wars_app/src/core/widgets/styles/custom_text_styles.dart';
+import 'package:star_wars_app/src/modules/home/presentation/pages/home_page.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+  static const String routePath = "/";
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  final PageController _pageController = PageController(initialPage: 0);
+  int _selectedIndex = 0;
+  var titles = ["Star Wars", "Liked"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          titles.elementAt(_selectedIndex),
+          style: starWarsFont(fontSize: 24, color: Colors.amber)
+              .merge(customOutlinedTextStyle()),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      extendBody: true,
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: const [HomePage(), Text('test liked')],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        height: kBottomNavigationBarHeight,
+        color: CustomColors.primaryColor,
+        notchMargin: 6,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+              width: 100,
+              height: kBottomNavigationBarHeight,
+              child: InkWell(
+                child: Icon(
+                  Icons.home,
+                  color: _selectedIndex == 0 ? Colors.black : Colors.white,
+                ),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 0;
+                    _pageController.jumpToPage(_selectedIndex);
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              width: 100,
+              height: kBottomNavigationBarHeight,
+              child: InkWell(
+                child: Icon(
+                  CupertinoIcons.heart_fill,
+                  color: _selectedIndex == 1 ? Colors.black : Colors.white,
+                ),
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = 1;
+                    _pageController.jumpToPage(_selectedIndex);
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
