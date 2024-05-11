c6755e871f28b397c0c209051d8966649a5dd9d2
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-05-12 00:46:41 +0800

    Presentation: Starship Bloc
    - added loading by ids for starship, this gets an event of list of urls, then calls the get current starship use case for each of the ids of those urls, finally returning a list of starship entities

--- lib/src/modules/home/presentation/blocs/starship_bloc/starship_bloc.dart
+++ lib/src/modules/home/presentation/blocs/starship_bloc/starship_bloc.dart
@@ -4,26 +4,39 @@ import 'package:equatable/equatable.dart';
 import 'package:star_wars_app/src/core/resources/data_state.dart';
 
 import 'package:star_wars_app/src/core/utils/extensions/string_extensions.dart';
+import 'package:star_wars_app/src/modules/home/domain/entities/responses/list_starship_response_entity.dart';
 import 'package:star_wars_app/src/modules/home/domain/entities/starship_entity.dart';
 import 'package:star_wars_app/src/modules/home/domain/use_cases/starship/get_current_starship_use_case.dart';
 import 'package:star_wars_app/src/modules/home/domain/use_cases/starship/list_starship_use_case.dart';
+import 'package:star_wars_app/src/modules/home/presentation/blocs/person_bloc/person_bloc.dart';
 
 part 'starship_event.dart';
 part 'starship_state.dart';
 
 class StarshipBloc extends Bloc<StarshipEvent, StarshipState> {
   final GetCurrentStarshipUseCase _getCurrentStarshipUseCase;
-  // ignore: unused_field
   final ListStarshipUseCase _listStarshipUseCase;
   StarshipBloc(
     this._getCurrentStarshipUseCase,
     this._listStarshipUseCase,
   ) : super(StarshipInitial()) {
-    on<StarshipEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<StarshipEvent>((event, emit) {});
     on<OnGetStarshipsByIds>(_onGetStarshipsByIds);
+    on<OnStarshipPageChanged>(_onStarshipPageChanged);
   }
+  _onStarshipPageChanged(
+      OnStarshipPageChanged event, Emitter<StarshipState> emit) async {
+    emit(StarshipLoading());
+    final result = await _listStarshipUseCase.call(params: event.pageNumber);
+
+    if (result is DataSuccess) {
+      emit(ListOfStarshipsLoaded(result: result.data));
+    }
+    if (result is DataFailure) {
+      emit(StarshipError(exception: result.exception!));
+    }
+  }
+
   _onGetStarshipsByIds(
       OnGetStarshipsByIds event, Emitter<StarshipState> emit) async {
     emit(StarshipLoading());
