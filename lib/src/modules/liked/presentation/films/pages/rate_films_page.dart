9e7b622dc227283f8cc9cc6fc0bd88545d1c58a9
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-05-20 00:35:09 +0800

    Films UI: rate films page
    - Added background for rate films page
    - Enhanced (not finished) rate films page
    - Added image to pubspec

--- lib/src/modules/liked/presentation/films/pages/rate_films_page.dart
+++ lib/src/modules/liked/presentation/films/pages/rate_films_page.dart
@@ -16,30 +16,42 @@ class RateFilmsPage extends StatefulWidget {
 class _RateFilmsPageState extends State<RateFilmsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Films',
-          style: starWarsFont(color: Colors.amber, fontSize: 16)
-            ..merge(customOutlinedTextStyle()),
+    return Stack(children: [
+      Image.asset(
+        'assets/images/space_background.jpg',
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.fill,
+      ),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.white,
+          title: Text(
+            'Rank These Films',
+            style: starWarsFont(color: Colors.amber, fontSize: 24)
+                .merge(customOutlinedTextStyle()),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
+        // backgroundColor: Colors.grey,
+        body: BlocBuilder<LocalFilmBloc, LocalFilmState>(
+          builder: (context, state) {
+            if (state is LocalFilmsLoaded) {
+              var listOfFIlms = state.films;
+              return _buildReorderableFilmList(listOfFIlms);
+            }
+            return const CupertinoActivityIndicator();
+          },
+        ),
       ),
-      body: BlocBuilder<LocalFilmBloc, LocalFilmState>(
-        builder: (context, state) {
-          if (state is LocalFilmsLoaded) {
-            var listOfFIlms = state.films;
-            return _buildReorderableFilmList(listOfFIlms);
-          }
-          return const CupertinoActivityIndicator();
-        },
-      ),
-    );
+    ]);
   }
 
   Widget _buildReorderableFilmList(List<FilmEntity> films) {
     return ReorderableListView(
-        //
+        padding: const EdgeInsets.all(12),
         onReorder: (int oldIndex, int newIndex) {
           setState(() {
             if (oldIndex < newIndex) {
@@ -58,12 +70,60 @@ class _RateFilmsPageState extends State<RateFilmsPage> {
         },
         children: <Widget>[
           for (int i = 0; i < films.length; i++)
-            ListTile(
+            Card(
+              color: Colors.grey.shade700.withOpacity(0.5),
+              elevation: 12,
               key: ValueKey('$i'),
-              title: Text(films[i].title),
-              trailing: ReorderableDragStartListener(
-                index: i,
-                child: const Icon(Icons.drag_handle),
+              child: Padding(
+                padding: const EdgeInsets.only(right: 8, top: 4, bottom: 4),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Stack(alignment: Alignment.center, children: [
+                      Icon(
+                        Icons.star,
+                        size: 64,
+                        color: _getStarColor(i),
+                      ),
+                      Text(
+                        '${i + 1}',
+                        style: openSansMedium(color: Colors.white, fontSize: 20)
+                            .merge(customOutlinedTextStyle()),
+                      )
+                    ]),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Container(
+                        width: 64,
+                        height: 84,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: Image.asset(
+                              'assets/images/no_image.jpg',
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ))),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Expanded(
+                        child: Text(
+                      films[i].title,
+                      style: openSansMedium(color: Colors.white, fontSize: 18),
+                    )),
+                    ReorderableDragStartListener(
+                      index: i,
+                      child: Icon(
+                        CupertinoIcons.move,
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
         ]);
@@ -73,4 +133,24 @@ class _RateFilmsPageState extends State<RateFilmsPage> {
     LocalFilmBloc bloc = LocalFilmBloc(sl());
     bloc.add(OnSaveOrUpdateFilms(films: films));
   }
+
+  Color _getStarColor(index) {
+    if (index == 0) {
+      return Colors.amber;
+    }
+    if (index == 1) {
+      return Colors.blueGrey;
+    }
+    if (index == 2) {
+      return Colors.brown.shade300;
+    }
+    if (index >= 6) {
+      return Colors.red;
+    }
+    return Colors.grey;
+  }
+
+  // Widget _buildFilmCard(FilmEntity film) {
+
+  // }
 }
