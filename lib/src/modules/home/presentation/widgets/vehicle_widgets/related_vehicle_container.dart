f24b86323dff0d54b2d437ce97fcaf9a51daf06b
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-05-22 01:26:59 +0800

    Vehicle Widgets: card and related
    - card: added a flag to display if model will be displayed; and added a safety checker for image.network
    - related: added no mdata widget if list of vehicles is empty and added vehicle card, also added spacers and paddings

--- lib/src/modules/home/presentation/widgets/vehicle_widgets/related_vehicle_container.dart
+++ lib/src/modules/home/presentation/widgets/vehicle_widgets/related_vehicle_container.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:star_wars_app/src/core/widgets/styles/custom_text_styles.dart';
 import 'package:star_wars_app/src/modules/home/domain/entities/vehicle_entity.dart';
 import 'package:star_wars_app/src/modules/home/presentation/blocs/vehicle_bloc/vehicle_bloc.dart';
+import 'package:star_wars_app/src/modules/home/presentation/widgets/no_data_widget.dart';
 import 'package:star_wars_app/src/modules/home/presentation/widgets/vehicle_widgets/vehicle_card.dart';
 
 class RelatedVehicleContainer extends StatelessWidget {
@@ -17,23 +19,29 @@ class RelatedVehicleContainer extends StatelessWidget {
           'Vehicles Used/Owned',
           style: openSansBoldText(fontSize: 16),
         ),
+        const SizedBox(
+          height: 12,
+        ),
         BlocBuilder<VehicleBloc, VehicleState>(builder: (context, state) {
           if (state is VehicleLoading) {
             return const Center(child: CupertinoActivityIndicator());
           }
           if (state is VehicleByIdsLoaded && state.vehicles!.isEmpty) {
-            return const Center(
-              child: Text('No Vehicles'),
-            );
+            return const NoDataWidget(
+                icon: Icons.car_repair, message: 'No Vehicles Found');
           }
           if (state is VehicleByIdsLoaded && state.vehicles!.isNotEmpty) {
             List<VehicleEntity>? vehicles = state.vehicles;
             return ListView.builder(
                 physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.all(8),
                 shrinkWrap: true,
                 itemCount: vehicles!.length,
                 itemBuilder: ((context, index) {
-                  return VehicleCard(vehicle: vehicles[index]);
+                  return VehicleCard(
+                    vehicle: vehicles[index],
+                    isLarge: false,
+                  );
                 }));
           }
           return Container();
